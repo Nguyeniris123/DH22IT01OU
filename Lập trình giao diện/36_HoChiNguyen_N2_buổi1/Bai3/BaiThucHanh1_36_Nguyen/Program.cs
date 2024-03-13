d54afd2887f911c0c84e9d18bd49d8ad42893f80
Hồ Chí Nguyên <136091628+Nguyeniris123@users.noreply.github.com>
2024-03-13 22:46:58 +0700

    Fix bài 3 ltgd

---
+++ Lập trình giao diện/36_HoChiNguyen_N2_buổi1/Bai3/BaiThucHanh1_36_Nguyen/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiThucHanh1_36_Nguyen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap So thu nhat: "); // read first number from user
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap So thu hai: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int tong = a + b;
+            Console.WriteLine("Tong cua {0} và {1} la: {2}", a, b, tong);
+            Console.Read();
+        }
+    }
+}
