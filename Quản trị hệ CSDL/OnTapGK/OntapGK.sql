508f45d5c5e83fa1f076aa721c624dbd0664922a
Hồ Chí Nguyên <136091628+Nguyeniris123@users.noreply.github.com>
2024-09-12 10:55:00 +0700

    csdl

---
+++ Quản trị hệ CSDL/OnTapGK/OntapGK.sql
@@ -0,0 +1,55 @@
+﻿use master
+Create database QLSV
+
+use QLSV
+create table SinhVien 
+(
+	MaSV char(11) Primary key,
+	HoSV nvarchar(20),
+	TenSV nvarchar(50)
+)
+
+insert into SinhVien
+values ('2251050048', 'Ho Chi', 'Nguyen')
+
+select * from SinhVien
+
+Backup database QLSV 
+to disk = 'C:\OnTap\QLSV.bak'
+
+insert into SinhVien
+values ('SV2', 'Lê Văn', 'Bình')
+
+Backup database QLSV 
+to disk = 'C:\OnTap\QLSV.bak'
+with differential
+
+insert into SinhVien
+values ('SV1', 'Nguyễn Thị', 'Ánh')
+select * from SinhVien
+
+UPDATE SinhVien
+SET HoSV = N'Nguyễn Thị', TenSV = N'Ánh'
+WHERE MaSV = 'SV1';
+
+Backup Log QLSV 
+to disk = 'C:\OnTap\QLSV.bak'
+with no_truncate
+
+use master
+drop database QLSV
+
+restore database QLSV
+from disk = 'C:\OnTap\QLSV.bak'
+with file = 1, norecovery
+
+restore database QLSV
+from disk = 'C:\OnTap\QLSV.bak'
+with file = 2, norecovery
+
+restore Log QLSV
+from disk = 'C:\OnTap\QLSV.bak'
+with file = 3, recovery
+
+use QLSV
+select * from SinhVien
